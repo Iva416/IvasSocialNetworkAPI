98dc1d3ff62bc0ab27b1480c4eccb72e8c5e84e7
Iva416 <112923663+Iva416@users.noreply.github.com>
2022-11-13 10:55:20 -0500

    Updated index.js

--- routes/api/userroutes.js
+++ routes/api/userroutes.js
@@ -6,7 +6,7 @@ const {
   postNewuser,
   deleteUser,
   addReaction,
-} = require('../controllers/usercontroller');
+} = require('../../controllers/usercontroller');
 
 router.route('/').get(getAlluser).post(postNewuser);
 
